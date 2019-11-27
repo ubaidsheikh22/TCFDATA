fbee48b13107cb53579a47c818e5850547285841
ubaid sheikh <ubaidsheikh.iu@gmail.com>
2019-11-27 22:16:02 +0500

    project list and upload file activity

--- src/main/java/com/example/tcfapplication/ProjectList.java
+++ src/main/java/com/example/tcfapplication/ProjectList.java
@@ -3,12 +3,33 @@ package com.example.tcfapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
 
 public class ProjectList extends AppCompatActivity {
 
+    String campusname[]=new String[]{"Karachi","Lahore","Hydrabad","Sukkhar","Dadu","Islamabad"};
+    String Location[]=new String[]{"Karachi","Lahore","Hydrabad","Sukkhar","Dadu","Islamabad"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project_list);
+
+        ListView listView = findViewById(R.id.list_view);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,campusname);
+        listView.setAdapter(adapter);
+
+
+        ListView listView1 = findViewById(R.id.list_view1);
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,Location);
+        listView1.setAdapter(adapter1);
+
+
+        Spinner spinner =findViewById(R.id.dropdown);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,
+                getResources().getStringArray(R.array.Region));
+        adapter2.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinner.setAdapter(adapter2);
     }
 }
--- src/main/java/com/example/tcfapplication/Sitevisite.java
+++ src/main/java/com/example/tcfapplication/Sitevisite.java
@@ -2,13 +2,87 @@ package com.example.tcfapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 public class Sitevisite extends AppCompatActivity {
 
+    private static int RESULT_LOAD_IMAGE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sitevisite);
+
+
+        Spinner spinner = findViewById(R.id.selregdrop);
+        ArrayAdapter<String> regionadapter =new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,getResources().getStringArray(R.array.Region));
+        regionadapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinner.setAdapter(regionadapter);
+
+
+
+        Spinner spinner1 = findViewById(R.id.sellocdrop);
+        ArrayAdapter<String> locdrop = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,getResources().getStringArray(R.array.Location));
+        regionadapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinner1.setAdapter(locdrop);
+
+
+        Spinner spinner2 = findViewById(R.id.browsimage);
+        ArrayAdapter<String> browsimagedrop = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,getResources().getStringArray(R.array.Images));
+        regionadapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinner2.setAdapter(browsimagedrop);
+
+
+        Button buttonLoadImage = (Button) findViewById(R.id.chosefile);
+        buttonLoadImage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent i = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            ImageView imageView = (ImageView) findViewById(R.id.imageview);
+            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+
+        }
+
+
     }
 }
+
+
+
